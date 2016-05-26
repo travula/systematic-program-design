3d105ad46bfda358f03e2c32c378f272f4bc205a
Thirumal Ravula <travula@vlabs.ac.in>
2016-05-26 14:05:41 +0530

    renamed the directories

---
+++ 1b-htdf/double-starter.rkt
@@ -0,0 +1,17 @@
+#lang racket
+
+;PROBLEM:
+
+;Design a function that consumes a number and produces twice that number.  Call
+;;your function double.  Follow the HtDF recipe and leave behind commented out
+;versions of the stub and template.
+
+;    HTDF RECIPE
+;    Signature, purpose and stub.
+;    Define examples, wrap each in check-expect.
+;    Template and inventory.
+;    Code the function body.
+;    Test and debug until correct
+
+;; Number -> Number
+;; 
